d2a7b51e1915c2787382e9822f1268cbae76ff9b
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-06-17 13:41:31 +0000

    Update Flake to 22.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Python template project";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
 
   outputs = { self, nixpkgs }: {
     devShell = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix (system:
