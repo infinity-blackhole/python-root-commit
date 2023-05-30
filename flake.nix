ebedcacce361fbd70834e894a98b011b77b59767
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-05-30 17:33:28 +0000

    Add well known base config

--- flake.nix
+++ flake.nix
@@ -38,6 +38,7 @@
               packages = [
                 pkgs.nixpkgs-fmt
                 pkgs.docker
+                pkgs.nodejs
               ];
             }
             {
@@ -45,6 +46,10 @@
                 black.enable = true;
                 isort.enable = true;
               };
+              packages = [
+                pkgs.hatch
+                pkgs.python3
+              ];
             }
           ];
         };
